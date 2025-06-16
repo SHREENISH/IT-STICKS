7a9426781370e506fccf75ac72a9f0fd05dda9ca
Shreenish Kumar Shakya <shreenishshakya025@gmail.com>
2025-06-16 17:23:16 +0545

    login/ sign up system
    i added a simple login and signup system for my website

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, redirect, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask(__name__)
+app.secret_key = '5HR33N15H#1s'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+#user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+#home/dashboard
+
+@app.route('/')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    return render_template('dashboard.html', username=session['username'])
+
+
+#register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+
+        if User.query.filter_by(username=username).first():
+            return 'Username already exists'
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('/login'))
+    
+    return render_template('register.html')
+
+
+#Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username,).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            session['username'] = user.username
+            return redirect('/')
+        else:
+            return 'Invalid credebntials'
+
+    return render_template('login.html')
+
+
+#logout route
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
