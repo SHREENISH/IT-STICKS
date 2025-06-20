e534ceef3bbdb6d6cd2d3a10f98f4f85fa75d3be
Shreenish Kumar Shakya <shreenishshakya025@gmail.com>
2025-06-20 21:50:50 +0545

    soime changes

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    profile_pic = db.Column(db.String(150), nullable=True)
+    profile_pic = db.Column(db.String(150), nullable=False, default='default.png')
 
 #home/dashboard
 
